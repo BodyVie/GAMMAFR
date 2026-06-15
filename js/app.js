d0a10f62429be3524b0d1cbc0e63f99d750963b2
Body <anthony.03052015@gmail.com>
2026-06-16 01:18:48 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -175,9 +175,9 @@
 
   // ---- modèle -------------------------------------------------------------
   var LEVELS = [
-    { id: "base",  label: "GAMMA base",  desc: "Uniquement la traduction de base, aucun patch." },
-    { id: "tweak", label: "GAMMA tweak", desc: "Base incluse + choix parmi les patchs Tweak." },
-    { id: "extra", label: "GAMMA extra", desc: "Base incluse + choix parmi les patchs Tweak et Extra." }
+    { id: "base",  label: "G.A.M.M.A. base",  desc: "Patch français couvrant uniquement G.A.M.M.A. de base (0 mod supplémentaire comme sortie d'installation)." },
+    { id: "tweak", label: "G.A.M.M.A. tweak", desc: "Patch français couvrant uniquement G.A.M.M.A. de base & l'ensemble des mods désactivés à l'installation de G.A.M.M.A." },
+    { id: "extra", label: "G.A.M.M.A. extra", desc: "Patch français couvrant uniquement G.A.M.M.A. de base & l'ensemble des mods désactivés à l'installation de G.A.M.M.A. & bien d'autres mod externe à G.A.M.M.A. !\nSi vous souhaitez ajouter une traduction pour un mod non couvert par le Pack, Rendez-vous dans l'onglet < CONTACT > afin de nous le suggérer." }
   ];
 
   function tagSection(section) {
